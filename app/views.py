9284d0335d39dd417d1aa2335438752c4a5d1169
evantoh <evanmwenda@gmail.com>
2018-01-23 22:48:52 +0300

    implement dynamic variables

--- app/views.py
+++ app/views.py
@@ -11,3 +11,6 @@ def index():
     '''
     message = 'Wewe ni simple'
     return render_template('index.html',message = message)
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    return render_template('movie.html',id = movie_id)
