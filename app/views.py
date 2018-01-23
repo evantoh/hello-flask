d6ef6b4f02a9d15086a95bd0cf4d55f73572fe54
evantoh <evanmwenda@gmail.com>
2018-01-23 22:16:30 +0300

    add basic parts of a flask app

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+
+#views
+# We import the render_template() function from Flask.
+#  This function takes in the name of a template file as the first argument
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
---
+++ run.py
@@ -0,0 +1,7 @@
+from app import app
+
+# app.run() method to launch our server. We pass in the debug = Trueargument 
+# to allow us to run on debug mode so that we can easily track errors in our 
+# application.
+if __name__=='__main__':
+    app.run(debug = True)
