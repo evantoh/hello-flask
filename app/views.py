c836a40e89b3b39cb30c71ef79dc61b6f9bf5613
evantoh <evanmwenda@gmail.com>
2018-01-23 23:26:34 +0300

    add control blocks and variable blocks

--- app/views.py
+++ app/views.py
@@ -9,8 +9,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Wewe ni simple'
-    return render_template('index.html',message = message)
+    title = 'Home-welcome to the best movie Review website Online'
+    return render_template('index.html',title = title)
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
     return render_template('movie.html',id = movie_id)
